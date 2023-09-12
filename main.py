6e54288cb37bbf305dcc437d0441daeae9051957
Eda DEMIRDERE <demirdereda@hotmail.com>
2023-09-12 14:45:36 +0300

    feature scaling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.impute import SimpleImputer
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 datas = pd.read_csv("satislar.csv")
 print(datas)
@@ -15,5 +16,16 @@ print(months)
 sales = datas[["Satislar"]]
 print(sales)
 
+# feature scaling
+x_train, x_test, y_train, y_test = train_test_split(months, sales, test_size=0.33, random_state=0)
+print("x train:", y_test)
+
+standart_scaler = StandardScaler()
+
+X_train = standart_scaler.fit_transform(x_train)
+X_test = standart_scaler.fit_transform(x_test)
+
+Y_train = standart_scaler.fit_transform(y_train)
+Y_test = standart_scaler.fit_transform(y_test)
 
 
